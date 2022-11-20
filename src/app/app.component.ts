ff5c4138f5e6dd02ef70cf9f1f0110e5ddc4fafc
Gabriel Simoes <gabrielsimoesy@gmail.com>
2022-11-20 12:33:47 -0400

    animations angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+
+
+//construção de animações
+const fadeInDown = trigger('openMobile', [ //gatilho para colocar no template html, (nome, definição)
+  state('in', style({ opacity:1,transform: 'translateY(0)' })),
+  transition('void => *', [
+  style({ opacity:0,transform: 'translateY(100%)' }),
+  animate(200)
+]),
+  transition('* => void', [
+  animate(200, style({ opacity:0,transform: 'translateY(100%)' }))
+])
+])
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    fadeInDown
+  ] //matriz onde declara as animações
 })
 export class AppComponent implements OnInit{
 
@@ -21,7 +39,7 @@ export class AppComponent implements OnInit{
     this.isMenuOpen = !this.isMenuOpen;
     console.log(this.isMenuOpen);
 
-
   }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContatoComponent } from './contato/contato.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
